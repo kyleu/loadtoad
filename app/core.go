39fc8f9b8a2cd7be2183a26524dffd3f4ffc318e
Kyle Unverferth <kyle@kyleu.com>
2024-03-17 08:08:30 -0600

    StringSlice util, packaging improvements, linting

---
+++ app/core.go
@@ -0,0 +1,28 @@
+// Package app - Content managed by Project Forge, see [projectforge.md] for details.
+package app
+
+import (
+	"context"
+
+	"github.com/kyleu/loadtoad/app/lib/exec"
+	"github.com/kyleu/loadtoad/app/lib/har"
+	"github.com/kyleu/loadtoad/app/lib/scripting"
+	"github.com/kyleu/loadtoad/app/lib/websocket"
+	"github.com/kyleu/loadtoad/app/util"
+)
+
+type CoreServices struct {
+	Har    *har.Service
+	Exec   *exec.Service
+	Script *scripting.Service
+	Socket *websocket.Service
+}
+
+func initCoreServices(ctx context.Context, st *State, logger util.Logger) CoreServices {
+	return CoreServices{
+		Har:    har.NewService(st.Files),
+		Exec:   exec.NewService(),
+		Script: scripting.NewService(st.Files, "scripts"),
+		Socket: websocket.NewService(nil, nil, nil),
+	}
+}
--- app/services.go
+++ app/services.go
@@ -3,24 +3,18 @@ package app // import github.com/kyleu/loadtoad
 import (
 	"context"
 
-	"github.com/kyleu/loadtoad/app/lib/exec"
 	"github.com/kyleu/loadtoad/app/lib/filesystem"
-	"github.com/kyleu/loadtoad/app/lib/har"
-	"github.com/kyleu/loadtoad/app/lib/scripting"
-	"github.com/kyleu/loadtoad/app/lib/websocket"
 	"github.com/kyleu/loadtoad/app/loadtoad"
 	"github.com/kyleu/loadtoad/app/util"
 )
 
 type Services struct {
-	Exec     *exec.Service
-	Socket   *websocket.Service
-	Script   *scripting.Service
-	Har      *har.Service
+	CoreServices
+
 	LoadToad *loadtoad.Service
 }
 
-func NewServices(_ context.Context, _ *State, logger util.Logger) (*Services, error) {
+func NewServices(ctx context.Context, st *State, logger util.Logger) (*Services, error) {
 	p := util.GetEnv("loadtoad_path", "./data")
 	fs, err := filesystem.NewFileSystem(p, false, "")
 	if err != nil {
@@ -31,12 +25,10 @@ func NewServices(_ context.Context, _ *State, logger util.Logger) (*Services, er
 		_ = fs.CreateDirectory(".")
 	}
 
-	ex := exec.NewService()
-	ws := websocket.NewService(nil, nil, nil)
-	sc := scripting.NewService(fs, "scripts")
-	hs := har.NewService(fs)
-	lt := loadtoad.NewService(fs, hs, ws, sc)
-	return &Services{Exec: ex, Socket: ws, Script: sc, Har: hs, LoadToad: lt}, nil
+	core := initCoreServices(ctx, st, logger)
+
+	lt := loadtoad.NewService(fs, core.Har, core.Socket, core.Script)
+	return &Services{CoreServices: core, LoadToad: lt}, nil
 }
 
 func (s *Services) Close(_ context.Context, _ util.Logger) error {
