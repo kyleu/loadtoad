2edb6fbd455d0a57801e6d88a4449393c039a7dc
Kyle Unverferth <kyle@kyleu.com>
2024-03-29 12:33:32 -0600

    Notarization and initialization improvments

--- app/controller/init.go
+++ app/controller/init.go
@@ -1,13 +1,16 @@
 package controller
 
 import (
+	"context"
+
 	"github.com/kyleu/loadtoad/app"
 	"github.com/kyleu/loadtoad/app/controller/cutil"
 	"github.com/kyleu/loadtoad/app/util"
 )
 
 // Initialize app-specific system dependencies.
-func initApp(_ *app.State, _ util.Logger) {
+func initApp(_ context.Context, _ *app.State, _ util.Logger) error {
+	return nil
 }
 
 // Configure app-specific data for each request.
